ee1eee662f7c4e2e68f7cca6242d0d56c7c9528e
Armando Monte <marmando@gmx.de>
2024-08-19 16:41:24 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: "/fungus/",
   plugins: [react()],
 })
